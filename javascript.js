7c3493e93987ad91152ce8253ec14d33d5410225
100495718 <100495718@alumnos.uc3m.es>
2024-12-12 03:22:03 +0100

    juegos

--- javascript.js
+++ javascript.js
@@ -54,29 +54,81 @@ const images = [
 ];
 
 let carouselIndex = 0; 
-// Referencias a los elementos del DOM
 const currentImage = document.getElementById("current-img");
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 
-// Función para actualizar la imagen mostrada
 function updateImage() {
   currentImage.src = images[currentIndex];
 }
 
-// Función para mostrar la imagen anterior
 function showPrevImage() {
   currentIndex = (currentIndex - 1 + images.length) % images.length; // Volver al último si es necesario
   updateImage();
 }
 
-// Función para mostrar la siguiente imagen
 function showNextImage() {
   currentIndex = (currentIndex + 1) % images.length; // Ir al inicio si es necesario
   updateImage();
 }
 
-// Event listeners para los botones
 prevButton.addEventListener("click", showPrevImage);
 nextButton.addEventListener("click", showNextImage);
 
+//Juegos
+
+let gift = Math.floor(Math.random() * 3) + 1; 
+function checkGift(treeNumber) { 
+  let result = document.getElementById("result"); 
+  if (treeNumber === gift) { 
+    result.textContent = "¡Felicidades! Encontraste el regalo oculto."; 
+    result.style.color = "green"; 
+  } 
+  else { 
+    result.textContent = "Sigue buscando... ¡No te rindas!"; 
+    result.style.color = "red"; 
+  } 
+  result.style.display = 'block';
+  setTimeout(clearResult, 2000);
+}
+
+function clearResult() { 
+  let result = document.getElementById("result"); 
+  result.style.display = 'none';
+}
+
+let score = 0; 
+let gameArea = document.getElementById('gameArea'); 
+let snowflake = document.getElementById('snowflake'); 
+let scoreDisplay = document.getElementById('score'); 
+let startButton = document.getElementById('startButton'); 
+let finalMessage = document.getElementById('finalMessage');
+
+startButton.addEventListener('click', startGame); 
+
+function startGame() { 
+  score = 0; 
+  scoreDisplay.textContent = 'Puntuación: ' + score; 
+  finalMessage.classList.add('hidden');
+  startButton.style.display = 'none'; 
+  moveSnowflake(); 
+  setTimeout(endGame, 30000); 
+}
+function moveSnowflake() {
+  let x = Math.floor(Math.random() * (gameArea.clientWidth - snowflake.clientWidth)); 
+  let y = Math.floor(Math.random() * (gameArea.clientHeight - snowflake.clientHeight)); 
+  snowflake.style.left = x + 'px'; 
+  snowflake.style.top = y + 'px'; 
+  snowflake.addEventListener('click', clickSnowflake); 
+} 
+function clickSnowflake() { 
+  score++; scoreDisplay.textContent = 'Puntuación: ' + score; 
+  moveSnowflake(); 
+} 
+
+function endGame() { 
+  snowflake.removeEventListener('click', clickSnowflake); 
+  startButton.style.display = 'block'; 
+  finalMessage.textContent = '¡Juego terminado! Puntuación final: ' + score; 
+  finalMessage.classList.remove('hidden');
+}
